88e7f3596428d7a80dbbd4de6ce58e3e3f2410aa
1000ch <orz1000ch@gmail.com>
2014-02-11 15:59:39 +0900

    add task

---
+++ refresh.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+from cobra.subscriptions import Subscriptions
+from cobra.store import Store
+
+# get feeds
+feeds = Subscriptions('cobra/static/subscriptions.xml').get_feeds()
+
+# connect to db
+list = []
+for feed in feeds:
+  for entry in feed.entries:
+    dic = {}
+    dic['feedTitle'] = feed.title
+    dic['htmlUrl'] = feed.htmlUrl
+    dic['xmlUrl'] = feed.xmlUrl
+    dic['title'] = entry.title
+    dic['link'] = entry.link
+    dic['date'] = entry.date
+    list.append(dic)
+
+store = Store()
+store.clear()
+store.insert(list)
