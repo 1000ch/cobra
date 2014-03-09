261ac7b9d8ca2deb35b9ac443a4ca0ae12fc32a4
1000ch <shogo.sensui@gmail.com>
2014-03-09 22:55:35 +0900

    wip

--- cobra/api.py
+++ cobra/api.py
@@ -1,14 +1,29 @@
 __author__ = '1000ch'
 
 from flask.ext import restful
+from flask.ext.restful import reqparse
 from cobra import app
-from cobra.store import Store
+from cobra.entry import EntryDAO
 
 api = restful.Api(app)
 
 class API(restful.Resource):
-  def get(self, skip):
-    list = Store().find({}, skip, 20)
-    return list
+  entryDao = EntryDAO()
 
-api.add_resource(API, '/api/<int:skip>')
+  parser = reqparse.RequestParser()
+  parser.add_argument('skip', type=int, help='Offset')
+  parser.add_argument('limit', type=int, help='Limit')
+
+  def get(self):
+
+    list = []
+    args = self.parser.parse_args()
+
+    if args.skip is not None and args.limit is not None:
+      list = self.entryDao.find({}, args.skip, args.limit)
+
+    print(list)
+    return {'entries': 0}
+
+#'/api/get/<int:skip>/<int:limit>'
+api.add_resource(API, '/api/get')
